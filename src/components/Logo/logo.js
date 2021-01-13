a3f783b8c6d745ae48bf8a8a49450473089891ad
Candice Barnsley <cebarnsley@gmail.com>
2021-01-13 18:38:33 +0000

    updated image pathing

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './logo.css';
+import  './logo.css';
+import foodChooserLogo from './foodChooserLogo.png'
 
 const Logo = () => {
 
  	return(
 		<div className="logoContainer" >
-			<img className="mainLogo" alt='food-chooser-logo' src={window.location.origin + '/images/foodChooserLogo.png'} />
+			<img className="mainLogo" alt='food-chooser-logo' src={foodChooserLogo} />
 		</div>
 		);
 }
